b0e496cae277cdd5dc7d30e5c87e67afeeef8c22
xcentric.starcraftfan <xcentric.starcraftfan@02bd71c7-5260-a1f9-b518-b30424e96287>
2010-12-23 03:00:20 +0000

    [*] CleanProjectDir.bat now removes all files from bin/debug and bin/release folders. [*] Important changes in SQL scripts. Now tables has fully FK-featured columns.

--- Resources/SQL Scripts/MySQL/CREATE Hardware.Tables GENERIC.sql
+++ Resources/SQL Scripts/MySQL/CREATE Hardware.Tables GENERIC.sql
@@ -12,23 +12,29 @@ CREATE TABLE IF NOT EXISTS `hardware_config`.`%TABLE_NAME%` (
   `RegistrationDateTime` DATETIME NOT NULL COMMENT 'Дата и время постановки на учёт.' ,
   `WarrantiedLifetime` DOUBLE NULL COMMENT 'Гарантийный срок службы.' ,
   `EstimatedLifetime` DOUBLE NULL COMMENT 'Расчётный (ожидаемый) срок службы.' ,
+  `WorkstationID` INT UNSIGNED NOT NULL COMMENT 'Рабочая станция, на которой обнаружен объект.' ,
+  `InventoryNumberID` INT UNSIGNED NOT NULL COMMENT 'Инвентарный номер.' ,
+  `MateriallyAccountableID` INT UNSIGNED NOT NULL COMMENT 'Материально ответственный.' ,
   %WMI_INFO% /* WMI Tech Info */
   PRIMARY KEY (`ID`) ,
-  INDEX `%TABLE_NAME%_WorkstationID` (`ID` ASC) ,
-  INDEX `%TABLE_NAME%_InventoryNumberID` (`ID` ASC) ,
-  INDEX `%TABLE_NAME%_MateriallyAccountableID` (`ID` ASC) ,
+  KEY `WorkstationID` (`WorkstationID`) ,
+  KEY `InventoryNumberID` (`InventoryNumberID`) ,
+  KEY `MateriallyAccountableID` (`MateriallyAccountableID`) ,
+  INDEX `%TABLE_NAME%_WorkstationID` (`WorkstationID` ASC) ,
+  INDEX `%TABLE_NAME%_InventoryNumberID` (`InventoryNumberID` ASC) ,
+  INDEX `%TABLE_NAME%_MateriallyAccountableID` (`MateriallyAccountableID` ASC) ,
   CONSTRAINT `%TABLE_NAME%_WorkstationID`
-    FOREIGN KEY (`ID` )
+    FOREIGN KEY (`WorkstationID` )
     REFERENCES `main`.`workstations` (`ID` )
     ON DELETE RESTRICT
     ON UPDATE CASCADE,
   CONSTRAINT `%TABLE_NAME%_InventoryNumberID`
-    FOREIGN KEY (`ID` )
+    FOREIGN KEY (`InventoryNumberID` )
     REFERENCES `main`.`inventory_numbers` (`ID` )
     ON DELETE RESTRICT
     ON UPDATE RESTRICT,
   CONSTRAINT `%TABLE_NAME%_MateriallyAccountableID`
-    FOREIGN KEY (`ID` )
+    FOREIGN KEY (`MateriallyAccountableID` )
     REFERENCES `main`.`users` (`ID` )
     ON DELETE RESTRICT
     ON UPDATE CASCADE)
--- Resources/SQL Scripts/MySQL/CREATE Software.Tables GENERIC.sql
+++ Resources/SQL Scripts/MySQL/CREATE Software.Tables GENERIC.sql
@@ -12,22 +12,28 @@ CREATE TABLE IF NOT EXISTS `software_config`.`installed_software` (
   `RegistrationDateTime` DATETIME NOT NULL COMMENT 'Дата и время постановки на учёт.' ,
   `LicenseType` TINYTEXT NULL COMMENT 'Тип лицензии.' ,
   `LicenseExpires` DATETIME NULL COMMENT 'Дата и время истечения лицензии.' ,
+  `WorkstationID` INT UNSIGNED NOT NULL COMMENT 'Рабочая станция, на которой обнаружен объект.' ,
+  `InventoryNumberID` INT UNSIGNED NOT NULL COMMENT 'Инвентарный номер.' ,
+  `MateriallyAccountableID` INT UNSIGNED NOT NULL COMMENT 'Материально ответственный.' ,
   PRIMARY KEY (`ID`) ,
-  INDEX `InstalledSoftware_WorkstationID` (`ID` ASC) ,
-  INDEX `InstalledSoftware_InventoryNumberID` (`ID` ASC) ,
-  INDEX `InstalledSoftware_MateriallyAccountableID` (`ID` ASC) ,
+  KEY `WorkstationID` (`WorkstationID`) ,
+  KEY `InventoryNumberID` (`InventoryNumberID`) ,
+  KEY `MateriallyAccountableID` (`MateriallyAccountableID`) ,
+  INDEX `InstalledSoftware_WorkstationID` (`WorkstationID` ASC) ,
+  INDEX `InstalledSoftware_InventoryNumberID` (`InventoryNumberID` ASC) ,
+  INDEX `InstalledSoftware_MateriallyAccountableID` (`MateriallyAccountableID` ASC) ,
   CONSTRAINT `InstalledSoftware_WorkstationID`
-    FOREIGN KEY (`ID` )
+    FOREIGN KEY (`WorkstationID` )
     REFERENCES `main`.`workstations` (`ID` )
     ON DELETE RESTRICT
     ON UPDATE CASCADE,
   CONSTRAINT `InstalledSoftware_InventoryNumberID`
-    FOREIGN KEY (`ID` )
+    FOREIGN KEY (`InventoryNumberID` )
     REFERENCES `main`.`inventory_numbers` (`ID` )
     ON DELETE RESTRICT
     ON UPDATE RESTRICT,
   CONSTRAINT `InstalledSoftware_MateriallyAccountableID`
-    FOREIGN KEY (`ID` )
+    FOREIGN KEY (`MateriallyAccountableID` )
     REFERENCES `main`.`users` (`ID` )
     ON DELETE RESTRICT
     ON UPDATE CASCADE)
@@ -36,23 +42,29 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `software_config`.`%TABLE_NAME%` (
   `ID` INT UNSIGNED NULL AUTO_INCREMENT ,
   `RegistrationDateTime` DATETIME NOT NULL COMMENT 'Дата и время постановки на учёт.' ,
+  `WorkstationID` INT UNSIGNED NOT NULL COMMENT 'Рабочая станция, на которой обнаружен объект.' ,
+  `InventoryNumberID` INT UNSIGNED NOT NULL COMMENT 'Инвентарный номер.' ,
+  `MateriallyAccountableID` INT UNSIGNED NOT NULL COMMENT 'Материально ответственный.' ,
   %WMI_INFO% /* WMI Tech Info */
   PRIMARY KEY (`ID`) ,
-  INDEX `%TABLE_NAME%_WorkstationID` (`ID` ASC) ,
-  INDEX `%TABLE_NAME%_InventoryNumberID` (`ID` ASC) ,
-  INDEX `%TABLE_NAME%_MateriallyAccountableID` (`ID` ASC) ,
+  KEY `WorkstationID` (`WorkstationID`) ,
+  KEY `InventoryNumberID` (`InventoryNumberID`) ,
+  KEY `MateriallyAccountableID` (`MateriallyAccountableID`) ,
+  INDEX `%TABLE_NAME%_WorkstationID` (`WorkstationID` ASC) ,
+  INDEX `%TABLE_NAME%_InventoryNumberID` (`InventoryNumberID` ASC) ,
+  INDEX `%TABLE_NAME%_MateriallyAccountableID` (`MateriallyAccountableID` ASC) ,
   CONSTRAINT `%TABLE_NAME%_WorkstationID`
-    FOREIGN KEY (`ID` )
+    FOREIGN KEY (`WorkstationID` )
     REFERENCES `main`.`workstations` (`ID` )
     ON DELETE RESTRICT
     ON UPDATE CASCADE,
   CONSTRAINT `%TABLE_NAME%_InventoryNumberID`
-    FOREIGN KEY (`ID` )
+    FOREIGN KEY (`InventoryNumberID` )
     REFERENCES `main`.`inventory_numbers` (`ID` )
     ON DELETE RESTRICT
     ON UPDATE RESTRICT,
   CONSTRAINT `%TABLE_NAME%_MateriallyAccountableID`
-    FOREIGN KEY (`ID` )
+    FOREIGN KEY (`MateriallyAccountableID` )
     REFERENCES `main`.`users` (`ID` )
     ON DELETE RESTRICT
     ON UPDATE CASCADE)
